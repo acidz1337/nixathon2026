6017b588cce65ca4f4d2d391bf3b9954581ca9ce
aargat44073 <alexander.argat@cengage.com>
2026-02-11 12:29:45 +0100

    Initial commit

---
+++ src/main/java/com/nixathon/nixathon/NixathonApplication.java
@@ -0,0 +1,13 @@
+package com.nixathon.nixathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NixathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NixathonApplication.class, args);
+	}
+
+}
